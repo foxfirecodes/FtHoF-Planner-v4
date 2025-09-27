50de8e7bf93fee0aaf1183475ca26bccfc71ac5c
foxfirecodes <foxfirecodes@gmail.com>
2025-09-26 23:06:19 -0400

    we live in a world of modules bb

--- src/base64.js
+++ src/base64.js
@@ -5,8 +5,7 @@
  *
  **/
 
-var Base64 = {
-
+export var Base64 = {
 	// private property
 	_keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
 
@@ -19,7 +18,6 @@ var Base64 = {
 		input = Base64._utf8_encode(input);
 
 		while (i < input.length) {
-
 			chr1 = input.charCodeAt(i++);
 			chr2 = input.charCodeAt(i++);
 			chr3 = input.charCodeAt(i++);
@@ -35,10 +33,12 @@ var Base64 = {
 				enc4 = 64;
 			}
 
-			output = output +
-				this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
-				this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
-
+			output =
+				output +
+				this._keyStr.charAt(enc1) +
+				this._keyStr.charAt(enc2) +
+				this._keyStr.charAt(enc3) +
+				this._keyStr.charAt(enc4);
 		}
 
 		return output;
@@ -54,7 +54,6 @@ var Base64 = {
 		input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
 
 		while (i < input.length) {
-
 			enc1 = this._keyStr.indexOf(input.charAt(i++));
 			enc2 = this._keyStr.indexOf(input.charAt(i++));
 			enc3 = this._keyStr.indexOf(input.charAt(i++));
@@ -72,13 +71,11 @@ var Base64 = {
 			if (enc4 != 64) {
 				output = output + String.fromCharCode(chr3);
 			}
-
 		}
 
 		output = Base64._utf8_decode(output);
 
 		return output;
-
 	},
 
 	// private method for UTF-8 encoding
@@ -87,12 +84,11 @@ var Base64 = {
 		var utftext = "";
 
 		for (var n = 0; n < string.length; n++) {
-
 			var c = string.charCodeAt(n);
 
 			if (c < 128) {
 				utftext += String.fromCharCode(c);
-			} else if ((c > 127) && (c < 2048)) {
+			} else if (c > 127 && c < 2048) {
 				utftext += String.fromCharCode((c >> 6) | 192);
 				utftext += String.fromCharCode((c & 63) | 128);
 			} else {
@@ -100,7 +96,6 @@ var Base64 = {
 				utftext += String.fromCharCode(((c >> 6) & 63) | 128);
 				utftext += String.fromCharCode((c & 63) | 128);
 			}
-
 		}
 
 		return utftext;
@@ -110,29 +105,28 @@ var Base64 = {
 	_utf8_decode: function (utftext) {
 		var string = "";
 		var i = 0;
-		var c = c1 = c2 = 0;
+		var c = (c1 = c2 = 0);
 
 		while (i < utftext.length) {
-
 			c = utftext.charCodeAt(i);
 
 			if (c < 128) {
 				string += String.fromCharCode(c);
 				i++;
-			} else if ((c > 191) && (c < 224)) {
+			} else if (c > 191 && c < 224) {
 				c2 = utftext.charCodeAt(i + 1);
 				string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
 				i += 2;
 			} else {
 				c2 = utftext.charCodeAt(i + 1);
 				c3 = utftext.charCodeAt(i + 2);
-				string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+				string += String.fromCharCode(
+					((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63),
+				);
 				i += 3;
 			}
-
 		}
 
 		return string;
-	}
-
-}
+	},
+};
